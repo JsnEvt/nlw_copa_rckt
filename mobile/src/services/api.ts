d4e89bb43fae831df1341a82b06a2416a618020a
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2024-05-29 15:44:29 -0300

    refactoring mobile folder

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://192.168.0.101:3333'
+    baseURL: 'http://192.168.0.39:3333'
     // baseURL:'http://vmi814957.contaboserver.net:3333'
     // baseURL:'http://192.168.100.93:3333'
 })
