7bc8db08b94214bee13a8dbe088a285c95b42953
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2024-05-29 16:55:45 -0300

    updating package.json / erro de autorizacao OAuth Google

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://192.168.0.39:3333'
+    // baseURL: 'http://192.168.0.39:8081'
+    baseURL: 'https://auth.expo.io/@jsnprogrammer77/nlwcopamobile'
     // baseURL:'http://vmi814957.contaboserver.net:3333'
     // baseURL:'http://192.168.100.93:3333'
 })
